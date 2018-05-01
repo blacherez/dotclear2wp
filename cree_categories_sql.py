2247ca46265b92bbd52be852426dd1c042701c9f
Benoit Lacherez <blacherez@gmail.com>
2018-05-01 20:44:43 +0200

    Création de la version SQL du script de création des catégories

---
+++ cree_categories_sql.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Crée les catégories qui n'existent pas encore dans le nouveau blog et enregistre un fichier json des correspondances d'id
+On utilise un fichier SQL pour les catégories de l'ancien blog et on utilise les fonctions du module cree_categories.py
+"""
+import re
+import cree_categories as cc
+#FILE = "data/categories.sql"
+FILE = "test_cat.sql"
+CATEGORY_FILE = "data/categories_from_sql.json"
+
+MOTIF_LIGNE = re.compile(r"(?P<cat_id>\d+), '(?P<blog_id>([^']*(\\'|'')?)*)', '(?P<cat_title>([^']*(\\'|'')?)*)', '(?P<cat_url>([^']*(\\'|'')?)*)', '(?P<cat_desc>([^']*(\\'|'')?)*)', (?P<cat_position>\d+), (?P<cat_lft>\d+), (?P<cat_rgt>\d+)")
+
+def parse_sql(line):
+    valeurs = {}
+    r = MOTIF_LIGNE.search(line)
+    if r:
+        valeurs["old_id"] = r.group("cat_id")
+        valeurs["name"] = r.group("cat_title")
+        valeurs["slug"] = r.group("cat_url")
+        valeurs["description"] = r.group("cat_desc")
+        return valeurs
+    return None
+
+if __name__ == "__main__":
+    # On récupère les catégories sur le site pour éviter de les créer en doublon
+    c = cc.get_remote_cats()
+    if c:
+        new_cats = c
+    else:
+        sys.exit(3)
+
+    with open(FILE) as f:
+        lines = f.readlines()
+
+    for line in lines:
+        valeurs = parse_sql(line)
+        if valeurs:
+            new_cats = cc.process(valeurs, new_cats)
+
+    cc.save2json(new_cats, CATEGORY_FILE)
