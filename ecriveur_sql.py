15d1b7560a3668ad157a505aceff6bad19331f1c
Benoit Lacherez <blacherez@gmail.com>
2018-05-03 21:10:20 +0200

    Suppression de code de test

--- ecriveur_sql.py
+++ ecriveur_sql.py
@@ -23,31 +23,13 @@ def parse_sql(line):
         valeurs["blog_id"] = val["blog_id"]
         return valeurs
 
-import re
-import codecs
-
-ESCAPE_SEQUENCE_RE = re.compile(r'''
-    ( \\U........      # 8-digit hex escapes
-    | \\u....          # 4-digit hex escapes
-    | \\x..            # 2-digit hex escapes
-    | \\[0-7]{1,3}     # Octal escapes
-    | \\N\{[^}]+\}     # Unicode characters by name
-    | \\[\\'"abfnrtv]  # Single-character escapes
-    )''', re.UNICODE | re.VERBOSE)
-
-def decode_escapes(s):
-    def decode_match(match):
-        return codecs.decode(match.group(0), 'unicode-escape')
-
-    return ESCAPE_SEQUENCE_RE.sub(decode_match, s)
-
 if __name__ == '__main__':
     nb = 0
     with open(FILE) as f:
         for l in f.readlines():
             billet = parse_sql(l)
             if billet:
-                print(decode_escapes(billet["title"]))
+                print(billet["title"])
                 a = e.process(billet, e.nouvelle_categorie, BLOG_ID)
                 if a:
                     nb += 1
